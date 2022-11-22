51d13715d58522335fffd7b04ef7f52219e6cb62
Abhijeet-Mandrekar <117968528+Abhijeet-Mandrekar@users.noreply.github.com>
2022-11-23 01:20:11 +0530

    Add files via upload
    Latest With Login and Logout System,  Styling is the same

---
+++ Home-page.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../Project/CSS/home-page-styling.css" />
+    
+    <!-- CSS only -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous" />
+
+      <script type="text/javascript">
+        function validateForm(thisform)
+        {
+            var s=thisform.Search.value;
+            if(s="" )
+            {
+              alert("Please Enter an Item");
+              thisform.Search.focus();
+              return false;
+            }
+            
+        }
+      </script>
+
+     
+
+<title>Welcome </title>
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid" >
+        <a class="navbar-brand" href="#" style="font-size: 5.5vh;">InvenTrack</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" 
+          aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="Search" id="navbarCollapse">
+          <form class="d-flex">
+            <input class="Search-Bar" type="text" placeholder="Search" aria-label="Search">
+            <input class="btn btn-outline-success" id="Button" type="submit" value="Search" style="background-color: rgb(7, 52, 186);
+              color: white; margin: 4px;"></input>
+          </form>
+
+          <a href="./logout.php">Logout  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" 
+              fill="currentColor" class="bi bi-power"  viewBox="-1 0 16 16">
+            <path d="M7.5 1v7h1V1h-1z"/>
+            <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
+          </svg></a>  
+        </div>
+      </div>
+    </nav>
+
+    <div class="sales">
+       <p> Welcome Back Administartor </p>
+       <hr>
+       <em> Total Sales Today </em>
+        <hr>
+        <?php
+          include './Partials/_dbconnect.php';
+          // query to add the sum of all the sales of today 
+        ?>
+    </div>
+
+    <div>
+      <ul>
+          <li><a href="./Home-page.php" >Home</a></li>
+          <li><a href="./Customer-page.php">Customers</a></li>
+          <li><a href="./Supplier-Page.php">Suppliers</a></li>
+          <li><a href="../Project/Items.html">Items</a></li>
+          <li><a href="../Project/Report.html">Report</a></li>
+          <li><a href="../Project/Aboutus.html">About Us</a></li>
+      </ul>
+    </div>
+  
+
+  </body>
+</html>
+
+
---
+++ login_page.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/login-styling.css">
+    
+    <title> Welcome to InvenTrack </title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    
+
+
+<script>
+function validateUrPw(thisform) 
+{ 
+	var userName= thisform.uname.value; 
+	var pass1= thisform.pw1.value; 
+	if(userName=="")
+	{
+		alert("Please enter the user name"); 
+		thisform.uname.focus();
+		return false;
+	}
+	if(pass=="") 
+	{
+		alert("Please enter the password"); 
+		thisform.pw1.focus(); 
+		return false;
+	}
+	return true;
+}
+</script>
+
+    <?php
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+        include './Partials/_dbconnect.php';
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        $sql = "Select * from admin where username ='$username' AND password='$password'";
+        $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
+
+        if($num == 1){  
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username ?>
+
+            <script>
+               alert('Logged In');
+            </script>
+        <?php    
+           header("location: ./Home-page.php");
+        }
+      }
+    ?>
+
+</head>
+
+<body>
+    
+    <!-- FOR BOOTSTRAP -->
+    <div id="carouselExampleSlidesOnly" class="image-box carousel-fade left-carousel" data-ride="carousel" data-interval="2000">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="image" src="./kisspng-inventory-management-software-warehouse-management-rack-vector-5b067d82259942.506115281527152002154.jpg" 
+                alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="image" src="./inventory-management-mistakes-6.jpg" alt="Second slide">
+          </div>
+        </div>
+      </div>
+    <div class="quote">
+        <p style="font-size: 18px;"> " The More Inventory a Company has, the Less Likely They will have what they need " </p>
+    </div>
+
+    <div class="login-box">
+     <p style="font-size: 8vh;"> InvenTrack</p>
+     <form action="./login_page.php" method="post"  onsubmit="return validateUrPw(this)">
+        <p style="font-size: 3vh; margin-top: 5px;">UserName</p>
+        <input type="text" id="emailid" placeholder="Username" name="username" required>
+        <p style="font-size: 3vh; margin-top: 5px;">Password</p>
+        <input type="password" id="pass" placeholder="Password" name="password" required>
+        <br><br>
+        <input type="submit" id="submit" value="Login" >
+    </form>
+    </div>    
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"   crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
